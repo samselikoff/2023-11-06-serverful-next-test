2cbe7dec2586416db45eae55864da0441ae4fb45
Sam Selikoff <sam.selikoff@gmail.com>
2023-11-06 17:26:09 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <p>hi</p>;
+import { sql } from "@vercel/postgres";
+
+export default async function Home() {
+  const { rows, fields } = await sql`SELECT * FROM pets`;
+
+  console.log(rows);
+
+  return (
+    <div>
+      {rows.map((row, i) => (
+        <p key={i}>
+          {row.name} - {row.owner}
+        </p>
+      ))}
+    </div>
+  );
 }
